1ae025df888ff020bf5bd92d680e8fc2c6bcf0df
Calira04 <carloseaslira@gmail.com>
2026-02-01 17:08:51 -0300

    test: valida função buscar_produto no main

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from modelos.produto import Produto
 from modelos.item_compra import Item
 from modelos.compra import Compra
+from modelos.gestor import Gerenciador
 
-# Produtos
-p1 = Produto("1", "Arroz", "Marca X", "kg", 5)
-p2 = Produto("2", "Feijão", "Marca Y", "kg", 1)
+ger = Gerenciador()
 
-# Compra
-minha_compra = Compra("31/01/2026", "Mercado Central")
+codigo_teste = 3
+resultado = ger.buscar_produto(codigo_teste)
 
-# Adicionando Itens
-minha_compra.adicionar_item(Item(p1, 25.00, 2)) # 2 pacotes de arroz
-minha_compra.adicionar_item(Item(p2, 8.50, 3))  # 3 pacotes de feijão
-
-# O GRANDE FINAL
-print(minha_compra)
+print(resultado)
