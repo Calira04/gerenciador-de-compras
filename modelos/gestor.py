04c20c4d844f847959ead551f8079d04b6ce0ffd
Calira04 <carloseaslira@gmail.com>
2026-02-01 20:28:24 -0300

    feat: implementa base da classe gerenciar

--- modelos/gestor.py
+++ modelos/gestor.py
@@ -3,47 +3,78 @@ from modelos.item_compra import Item
 from modelos.compra import Compra
 
 class Gerenciador:
+    '''
+    Classe para gerenciar produtos e compras.
+    '''
+
     def __init__(self):
+        '''
+        Inicializa o gerenciador com listas de produtos e compras.
+        '''
         self.produtos_cadastrados = []
-        self.itens_cadastrados = []
-
-        p1 = Produto("1", "Arroz", "Marca X", "kg", "5")
-        p2 = Produto("2", "Feijão", "Marca Y", "kg", "1")
-        
-        self.produtos_cadastrados.append(p1)
-        self.produtos_cadastrados.append(p2)
-
-        self.compra_atual = None
+        self.compras_cadastradas = []
 
     def adicionar_nova_compra(self):
-        pass
-        #pede o nome do mercado
-        #pede a data da compra
-        # cria uma lista de item vazia
-        #chama a def adicionar_novo_item() conforme o usuario e adiciona o retorno dela para a lista criada acima
-        # calcula total da compra chamando a funcao calcular funcao compra da classe compra
-        # retorna nova compra
+        '''
+        Cria uma nova compra, adiciona itens e guarda no histórico.
+        '''
+        nome_mercado = input('Digite o nome do mercado: ')
+        data_compra = input('Digite a data da compra: ')
+        
+        compra_atual = Compra(nome_mercado, data_compra)
+        
+        while True:
+            opcao = input("\nDigite 'sair' para finalizar ou qualquer tecla para adicionar item: ").lower()
+            if opcao == 'sair':
+                break
+            
+            item = self.adicionar_novo_item()
+            compra_atual.adicionar_item(item)
+            
+            print(f'Total acumulado: R$ {compra_atual.calcular_total_compra():.2f}')
+
+        self.compras_cadastradas.append(compra_atual)
+        print('\n--- COMPRA SALVA COM SUCESSO ---')
 
     def adicionar_novo_item(self):
-        codigo = int(input('Digite o codigo de barras: '))
+        '''
+        Solicita dados do item e cria um Item associado a um Produto.
+        '''
+        codigo = int(input('Digite o código de barras: '))
         produto = self.buscar_produto(codigo)
+        
         if produto is None:
             produto = self.adicionar_novo_produto(codigo)
+        
         valor_unitario = float(input('Digite o valor do item R$: '))
         quantidade = int(input('Digite a quantidade: '))
+        
         item = Item(produto, valor_unitario, quantidade)
+        
+        print('\nItem adicionado:')
+        print(item)
+        
         return item
 
     def adicionar_novo_produto(self, codigo):
-        nome_produto = str(input('Digite o nome do produto: '))
-        nome_marca = str(input('Digite o nome da marca: '))
-        unidade_medida = str(input('Digite a unidade de medida: '))
+        '''
+        Cria um novo produto e adiciona à lista de produtos cadastrados.
+        '''
+        nome_produto = input('Digite o nome do produto: ')
+        nome_marca = input('Digite o nome da marca: ')
+        unidade_medida = input('Digite a unidade de medida: ')
         qtd_unidade = float(input(f'Quanto(s) {unidade_medida}: '))
+        
         produto = Produto(codigo, nome_produto, nome_marca, unidade_medida, qtd_unidade)
         self.produtos_cadastrados.append(produto)
+        
         return produto
     
     def buscar_produto(self, codigo):
+        '''
+        Busca um produto pelo código de barras.
+        Retorna o Produto ou None se não existir.
+        '''
         for produto in self.produtos_cadastrados:
             if produto.codigo == codigo:
                 return produto
